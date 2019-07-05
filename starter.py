a5064e6ad9ac3e68ebc70d0681a64aa1d7968ca2
Sarah Townson <sarah.townson@scienceoxford.com>
2019-07-05 14:46:32 +0100

    Create starter.py

---
+++ starter.py
@@ -0,0 +1,14 @@
+from microbit import *
+
+# Please tag us if used!
+# We'd love to see what you make:
+# @ScienceOxford
+
+def right():
+    pin0.write_digital(1)
+def wrong():
+    pin0.write_digital(0)
+
+display.off()
+
+# Write new code below!
